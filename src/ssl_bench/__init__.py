3663d85fd86dbf913e747a1673cf2d3d27710130
Melvin Barbaux <melvin.barbaux@icloud.com>
2025-05-01 23:30:02 +0200

    Initial project scaffold (Python 3.12)

---
+++ src/ssl_bench/__init__.py
@@ -0,0 +1,2 @@
+\"\"\"SSL-Bench: Semi-Supervised Classification Benchmark Framework\"\"\"
+__version__ = "0.1.0"
---
+++ tests/test_smoke.py
@@ -0,0 +1,4 @@
+import ssl_bench
+
+def test_imports():
+    assert hasattr(ssl_bench, "__version__")
